366cdb6e414a1e3eed2e74cf24d626c2f7e44435
Elena Maslennikova <leen2@yandex.ru>
2023-04-28 09:13:25 +0300

    Sprint 3 - Проверка не только отображения элемента но и текста на нем, при переходе в разделах конструктора

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -10,18 +10,21 @@ def test_constructor_to_bread_success(web_driver):
     web_driver.find_element(By.XPATH, ConstructorLocator.SAUSE_LINK_XPATH).click()
     web_driver.find_element(By.XPATH, ConstructorLocator.BREAD_LINK_XPATH).click()
 
-    assert web_driver.find_element(By.XPATH, ConstructorLocator.BREAD_HEADER_XPATH).is_displayed()
+    bread_header = web_driver.find_element(By.XPATH, ConstructorLocator.BREAD_HEADER_XPATH)
+    assert bread_header.is_displayed() and bread_header.text == "Булки"
 
 
 def test_constructor_to_sause_success(web_driver):
     web_driver.get(PAGE_MAIN)
     web_driver.find_element(By.XPATH, ConstructorLocator.SAUSE_LINK_XPATH).click()
 
-    assert web_driver.find_element(By.XPATH, ConstructorLocator.SAUSE_HEADER_XPATH).is_displayed()
+    sause_header = web_driver.find_element(By.XPATH, ConstructorLocator.SAUSE_HEADER_XPATH)
+    assert sause_header.is_displayed() and sause_header.text == "Соусы"
 
 
 def test_constructor_to_filling_success(web_driver):
     web_driver.get(PAGE_MAIN)
     web_driver.find_element(By.XPATH, ConstructorLocator.FILLING_LINK_XPATH).click()
 
-    assert web_driver.find_element(By.XPATH, ConstructorLocator.FILLING_HEADER_XPATH).is_displayed()
+    fillings_header = web_driver.find_element(By.XPATH, ConstructorLocator.FILLING_HEADER_XPATH)
+    assert fillings_header.is_displayed() and fillings_header.text == "Начинки"
